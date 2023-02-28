42f47a77dc2c8d42962b8fbaa59474343e17533d
michaelyauhk01 <michaelyau@hk01.com>
2023-02-28 17:43:33 +0800

    create sheets

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const COL_COG = "單件COG"
 const COL_SETTLEMENT_PRICE = "結算價"
 const COL_ORDER_STATUS = "訂單狀態"
 
+const OrderSheetName = "工作表1"
+const MapMerchantSheetName = "MAP Merchant"
+const MapCogSheetName = "MAP COG"
+
 const OrderStatus = {
   MANUAL_CANCELLED: "已取消(主動)",
   AUTO_CANCELLED: "已取消(自動)",
@@ -55,20 +59,42 @@ function deleteOrderByStatus(sheet, status) {
     const orderStatus = data[i][invoiceStatusCellIdx]
 
     if (orderStatus === status) {
-      
+
       sheet.deleteRow(i + 1 - rowsDeleted)
       rowsDeleted++;
     }
   }
 }
 
+function sortInvoiceByStatus(sheet, status) {
+  if (!sheet) {
+    sheet = SpreadsheetApp.getActiveSheet();
+  }
+}
+
+function createSheet(sheetName) {
+  const spreadSheet = SpreadsheetApp.getActiveSpreadsheet()
+  const sheet = spreadSheet.getSheetByName(sheetName)
+
+  if (sheet) {
+    return;
+  }
+
+  spreadSheet.insertSheet(sheetName)
+}
+
 function main() {
-  const sheet = SpreadsheetApp.getActiveSheet()
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const orderSheet = spreadsheet.getSheetByName(OrderSheetName)
 
   // Add text [佣金] [單件COG] [結算價] at AQ2; AR2; AS2
-  appendColumns(sheet, [COL_COMMISSION, COL_COG, COL_SETTLEMENT_PRICE])
+  appendColumns(orderSheet, [COL_COMMISSION, COL_COG, COL_SETTLEMENT_PRICE])
 
   // Base on column O data, remove roll with 已取消(自動) 已取消(主動) in column O 
-  deleteOrderByStatus(sheet, OrderStatus.AUTO_CANCELLED)
-  deleteOrderByStatus(sheet, OrderStatus.MANUAL_CANCELLED)
+  deleteOrderByStatus(orderSheet, OrderStatus.AUTO_CANCELLED)
+  deleteOrderByStatus(orderSheet, OrderStatus.MANUAL_CANCELLED)
+
+  // Create two new tab with name [MAP Merchant] [MAP COG]
+  createSheet(MapMerchantSheetName)
+  createSheet(MapCogSheetName)
 }
